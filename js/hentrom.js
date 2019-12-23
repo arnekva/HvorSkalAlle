49542b8bedc9bdc9b70bc792ecf04cce546fe4b7
Arne Kvaleberg <arnekva@gmail.com>
2019-12-23 18:02:14 +0100

    Updated text if nothing is found

--- js/hentrom.js
+++ js/hentrom.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-$('#rom').html("Laster inn romnummer...");
+
 //legg til: get timeedit fra localstorage - add options
 let timeedit = "https://cloud.timeedit.net/hvl/web/studbergen/ri14X55Yg50041Q0g4QY5404Z565X572561Y540yZ06Q0675Y9X67701Z5cuZ505575Y2Q5X170YY1Zp770l5Q4xu06Q727l9Q5X047YL5W8l97Y1W03vwXEjWX47n66l51Xæq3ZnWQ1w52wa6a7178eWw7bn8tXpwc7WrWaa6K7QwmleXcbWcZrWj26en9rUWxclr7c7j8neEWmnæt71a7Wa83nWwwv1w2UXxnWr6tm7aWWXwX1rLb8bpc3j9X1XWw5Wrw6r705c1W92WcWX6nE5X6wav6WjnX6e6976eKWc9jj)nojtWa5XpXvvaxxcu6wW6v(xVwVXa5(nKcKvcowW75)X6Xj6dZ0wuaXwWr055WWY06k675QoZemQ9trtn16b61W1X5pdw47Q0ZD2043E63AF34CE619E2A055AA8B37EA7F.phtml"
 let fagkoder = ["DAT152", "DAT155", "DAT159"]
@@ -8,6 +8,7 @@ $.get( timeedit, function(response){
 
 $('#testDiv').html(response);
 let date = new Date()
+//TODO: Fix dato og tid
 let dato = "10.09.2019"//formatDate(date)
 let time = "10:15"//checkTime(date.getHours())
 console.log("Ja?")
@@ -36,7 +37,7 @@ for(let i = 0; i<dagensNoder.length;i++){
             $('[data-fag="'+ fagkoder[j] +'"]').append("<p>Type: " + dagensNoder[i].innerHTML.substr(16,5) + "</p>")
 
           }
-
+$('[data-fag="'+ fagkoder[j] +'"]').append("<hr>")
       }else{
 //nothing
 
@@ -46,13 +47,16 @@ for(let i = 0; i<dagensNoder.length;i++){
   }else{
     console.log("Adding one hour for " + i)
   time = (date.getHours()-9+t) + ":" + "15"
+  //TODO: Fjern -9
   }
   }
 
 
-//time = (date.getHours()+i) + ":" + "15"
-
-
+}
+for(k = 0;k<fagkoder.length;k++){
+if(document.querySelector('[title*=" ' + fagkoder[k] + '"]') == null){
+  $('[data-fag="'+ fagkoder[k] +'"]').append("<p class='ingenting'>Du har ingen timer i dag!</p>")
+}
 }
 /*
 for(let j = 0; j<3;j++){
